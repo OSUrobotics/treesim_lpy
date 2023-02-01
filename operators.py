624b67d60c65f3bb3f1a95ead1c46a030cd8cbbd
osu-youa <youa@oregonstate.edu>
2023-02-01 15:57:03 -0800

    initial attempt at 1-1 conversion between lstring and graph

--- operators.py
+++ operators.py
@@ -21,49 +21,118 @@ class TreeStructure:
 
         info = {}
 
+        # Find root - Only node with no predecessors and 1 successor
+        roots = [n for n in graph.nodes if len(list(graph.predecessors(n))) == 0 and len(list(graph.successors(n))) > 0]
+        assert len(roots) == 1
+        info['root'] = roots[0]
+
         return info
 
+    def to_lstring(self):
 
+        def explore(edge):
+
+            queued_modules = []
+            start_bid = self.graph.edges[edge]['branch_id']
+
+            while edge is not None:
+
+
+
+                data = self.graph.edges[edge]
+                queued_modules.extend(data.get('pre_modules', []))
+                queued_modules.append((data['name'], data['args']))
+                queued_modules.extend(data.get('post_modules', []))
+
+                node_info = self.graph.nodes[edge[1]]
+                if node_info['name']:
+                    queued_modules.append(self.graph.nodes[edge[1]])
+
+                next_edge = None
+                for node in self.graph.successors(edge[1]):
+                    new_edge = (edge[1], node)
+                    bid = self.graph.edges[new_edge]['branch_id']
+                    if bid == start_bid:
+                        next_edge = new_edge
+                    else:
+                        queued_modules.append(('[', ()))
+                        queued_modules.extend(explore(new_edge))
+                        queued_modules.append((']', ()))
+
+                edge = next_edge
+            return queued_modules
+
+        modules = []
+        root = self.info['root']
+        root_info = self.graph.nodes[root]
+        modules.append((root_info['name'], root_info.get('args', [])))
+
+        first_edge = (root, list(self.graph.successors(root))[0])
+        modules.extend(explore(first_edge))
+        lstring = lpy.Lstring()
+        for module_name, module_args in modules:
+            module = lpy.newmodule(module_name)
+            module.args = module_args
+            lstring.append(module)
+
+        return lstring
 
     @classmethod
-    def from_lstring(cls, lstring, node_modules, internode_modules):
+    def from_lstring(cls, lstring, node_modules, internode_modules, null_modules=None):
+
+        if null_modules is None:
+            null_modules = []
+
         final_graph = nx.DiGraph()
 
         last_node = None
-        queued_edge = {}
+
+        queued_modules = []
+        queued_internode = {}
 
         stack = []
         bid_counter = Counter()
         bid = bid_counter()
 
-        for module in lstring:
+        # TODO: Write out the directional modifiers separately
+
+        for i, module in enumerate(lstring):
             name = module.name
-            pid = id(module)
             if name in node_modules:
                 module_id = module.args[0]
                 assert module_id not in final_graph
-                final_graph.add_node(module_id, name=name, pid=pid, attribs=module.args[1:])
+                final_graph.add_node(module_id, name=name, args=module.args[1:])
                 if last_node is not None:
-                    final_graph.add_edge(last_node, module_id, **queued_edge)
+                    queued_internode['post_modules'] = queued_modules
+                    final_graph.add_edge(last_node, module_id, **queued_internode)
+                    queued_internode = {}
+                    queued_modules = []
                 last_node = module_id
             elif name in internode_modules:
-                queued_edge = {'name': name, 'pid': pid, 'attribs': module.args, 'branch_id': bid}
+                queued_internode = {'name': name, 'args': module.args, 'branch_id': bid, 'pre_modules': queued_modules}
+                queued_modules = []
+            elif name in null_modules:
+                assert not queued_internode
+
+                dummy_node = 'null_{}'.format(id(module))
+                final_graph.add_node(dummy_node, name='', args=[])
+                final_graph.add_edge(last_node, dummy_node, name=name, args=module.args, pre_modules=queued_modules, branch_id=bid)
+                queued_modules = []
+                bid = bid_counter()
+
             elif name == '[':
+                assert not queued_internode
                 stack.append((last_node, bid))
                 bid = bid_counter()
             elif name == ']':
+                if queued_internode:
+                    print('Issue at index {}: {}'.format(i, queued_internode))
+                    raise Exception()
                 last_node, bid = stack.pop()
+            else:
+                queued_modules.append((name, module.args))
 
         return cls(final_graph)
-
-
-
-
-
-
-
-
-
 
 
 class Counter:
@@ -75,36 +144,6 @@ class Counter:
         self.val += 1
         return cur_val
 
-def extract_skeleton_from_lstring(lstring, node_modules, internode_modules):
-
-    final_graph = nx.DiGraph()
-
-    last_node = None
-    queued_edge = {}
-
-    stack = []
-    bid_counter = Counter()
-    bid = bid_counter()
-
-    for module in lstring:
-        name = module.name
-        pid = id(module)
-        if name in node_modules:
-            module_id = module.args[0]
-            assert module_id not in final_graph
-            final_graph.add_node(module_id, name=name, pid=pid, attribs=module.args[1:])
-            if last_node is not None:
-                final_graph.add_edge(last_node, module_id, **queued_edge)
-            last_node = module_id
-        elif name in internode_modules:
-            queued_edge = {'name': name, 'pid': pid, 'attribs': module.args, 'branch_id': bid}
-        elif name == '[':
-            stack.append((last_node, bid))
-            bid = bid_counter()
-        elif name == ']':
-            last_node, bid = stack.pop()
-
-    return final_graph
 
 def insert_modules(lstring, module_to_insert, node_pairs, node_modules, at_end=False):
 
@@ -205,7 +244,8 @@ def tie_all(lstring):
 
 
 if __name__ == '__main__':
-    import matplotlib.pyplot as plt
+    pass
+    # import matplotlib.pyplot as plt
     # curve = OGH([0, 0], [1, 1], [5, 0], [1, 0])
     # pts = curve.eval()
     #
@@ -213,20 +253,20 @@ if __name__ == '__main__':
     # plt.axis('equal')
     # plt.show()
     #
-    curve = OGH([0, 0, 0], [1, 0, 0], [3, 3, 3], [0, 1, 0])
-    pts = curve.eval()
-
-    ax = plt.figure().add_subplot(projection='3d')
-    ax.plot(pts[:,0], pts[:,1], pts[:,2])
-    plt.show()
-
-    curve_1 = OGH([0, 0], [1, 1], [5, 0], [1, 0])
-    curve_2 = OGH([0, 0], [1, 1], [10, 0], [1, 0])
-    curve_3 = OGH([0, 0], [1, 1], [5, 0], [-1, -1])
-
-    print(f'Curve 1:\n\tStrain: {curve_1.strain}\n\tLength: {curve_1.approx_len}')
-    print(f'Curve 2:\n\tStrain: {curve_2.strain}\n\tLength: {curve_2.approx_len}')
-    print(f'Curve 3:\n\tStrain: {curve_3.strain}\n\tLength: {curve_3.approx_len}')
-
-    print()
-
+    # curve = OGH([0, 0, 0], [1, 0, 0], [3, 3, 3], [0, 1, 0])
+    # pts = curve.eval()
+    #
+    # ax = plt.figure().add_subplot(projection='3d')
+    # ax.plot(pts[:,0], pts[:,1], pts[:,2])
+    # plt.show()
+    #
+    # curve_1 = OGH([0, 0], [1, 1], [5, 0], [1, 0])
+    # curve_2 = OGH([0, 0], [1, 1], [10, 0], [1, 0])
+    # curve_3 = OGH([0, 0], [1, 1], [5, 0], [-1, -1])
+    #
+    # print(f'Curve 1:\n\tStrain: {curve_1.strain}\n\tLength: {curve_1.approx_len}')
+    # print(f'Curve 2:\n\tStrain: {curve_2.strain}\n\tLength: {curve_2.approx_len}')
+    # print(f'Curve 3:\n\tStrain: {curve_3.strain}\n\tLength: {curve_3.approx_len}')
+    #
+    # print()
+    #
