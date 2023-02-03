5d0a814582519e4804fc38aa51bad3968f06b742
osu-youa <youa@oregonstate.edu>
2023-02-03 15:52:00 -0800

    attempting to get tying logic down (still bug with second tie)

--- graph_utils.py
+++ graph_utils.py
@@ -7,7 +7,7 @@ import functools
 from openalea.plantgl.scenegraph.cspline import CSpline
 
 
-class TreeStructure:
+class LStringGraphDual:
     def __init__(self, graph: nx.DiGraph):
 
         self.validate_graph(graph)
@@ -110,11 +110,11 @@ class TreeStructure:
             else:
                 self.graph.edges[edge]['pre_turtle_modules'] = []
 
-            if i == len(nodes) - 1:
+            if i == len(nodes) - 2:
                 self.graph.edges[edge]['post_turtle_modules'] = [('EndGuide', [])]
+                self.graph.edges[edge]['post_modules'].append(('Flags', [1]))
             else:
                 self.graph.edges[edge]['post_turtle_modules'] = []
-
 
     def set_node_attributes(self, mapping, name=None):
         nx.set_node_attributes(self.graph, mapping, name=name)
@@ -258,51 +258,6 @@ class Counter:
         cur_val = self.val
         self.val += 1
         return cur_val
-
-
-
-def tie_at(lstring, idx, guide, depth=1):
-    """
-    Given an L-String, the index of the branch to be guided, and a curve to be followed,
-    update the existing guide.
-    """
-
-    # Check if there is a rotation/existing guide following the current object
-    # If so, remove it
-    for _ in range(depth):
-        if lstring[idx + 1].name in ['&', '/', 'SetGuide']:
-            del lstring[idx + 1]
-
-    lstring.insertAt(idx + 1, guide)
-
-
-
-def tie_all(lstring):
-    for idx, lsys_elem in enumerate(lstring):
-        try:
-            wood = lsys_elem[0].type
-        except (TypeError, AttributeError, IndexError):
-            continue
-
-        if not isinstance(wood, BasicWood) or not wood.can_tie:
-            continue
-
-        # If an element has not had its tie updated, we continue to let it grow out
-        if not wood.tie_updated or not wood.guide_points:
-            continue
-
-        guide = wood.produce_tie_guide()
-        wood.tie_updated = False
-
-        depth = 1
-        if not wood.has_tied:
-            depth = 2
-            wood.has_tied = True
-
-        tie_at(lstring, idx, guide, depth=depth)
-
-        return True
-    return False
 
 
 if __name__ == '__main__':
